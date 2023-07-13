b00c8ff336973befd88904c4ca8e501105033964
Sana-0511 <svnkanbargi@gmail.com>
2023-07-13 16:41:15 +0530

    usersworking

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,16 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { db } from "./firebaseConfig.js";
 
-
 export default function ContactTable() {
   const [contactData, setContactData] = useState([]);
-//   const [editPropertyId, setEditPropertyId] = useState("");
-//   const [editPropertyName, setEditPropertyName] = useState("");
-//   const [editPropertyType, setEditPropertyType] = useState("");
-//   const [editPropertyAddress, setEditPropertyAddress] = useState("");
-//   const [editPropertyArea, setEditPropertyArea] = useState("");
-//   const [editPropertyMapLocation, setEditPropertyMapLocation] = useState("");
-//   const [editPropertyState, setEditPropertyState] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -22,7 +14,7 @@ export default function ContactTable() {
       const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
       setContactData(data);
     } catch (error) {
-      console.log("Error fetching property data:", error);
+      console.log("Error fetching contact data:", error);
     }
   };
 
@@ -31,46 +23,9 @@ export default function ContactTable() {
       await db.collection("ContactUsRecord").doc(id).delete();
       fetchData();
     } catch (error) {
-      console.log("Error deleting property:", error);
+      console.log("Error deleting contact:", error);
     }
   };
-
-//   const handleEdit = (property) => {
-//     setEditPropertyId(property.id);
-//     setEditPropertyName(property.propertyName);
-//     setEditPropertyType(property.propertyType);
-//     setEditPropertyAddress(property.address);
-//     setEditPropertyArea(property.area);
-//     setEditPropertyMapLocation(property.mapLocation);
-//     setEditPropertyState(property.state);
-//   };
-
-//   const handleCancelEdit = () => {
-//     setEditPropertyId("");
-//     setEditPropertyName("");
-//     setEditPropertyType("");
-//     setEditPropertyAddress("");
-//     setEditPropertyArea("");
-//     setEditPropertyMapLocation("");
-//     setEditPropertyState("");
-//   };
-
-//   const handleUpdateProperty = async () => {
-//     try {
-//       await db.collection("PropertyRecord").doc(editPropertyId).update({
-//         propertyName: editPropertyName,
-//         propertyType: editPropertyType,
-//         address: editPropertyAddress,
-//         area: editPropertyArea,
-//         mapLocation: editPropertyMapLocation,
-//         state: editPropertyState,
-//       });
-//       fetchData();
-//       handleCancelEdit();
-//     } catch (error) {
-//       console.log("Error updating property:", error);
-//     }
-//   };
 
   return (
     <div className="property-table-container">
@@ -89,23 +44,19 @@ export default function ContactTable() {
         <tbody>
           {contactData.map((contact) => (
             <tr key={contact.id}>
-              {contact.id ===  (
-                <>
-                  <td>{contact.firstName}</td>
-                  <td>{contact.lastName}</td>
-                  <td>{contact.email}</td>
-                  <td>{contact.phone}</td>
-                  <td>{contact.message}</td>
-                  <td>
-                    <button
-                      onClick={() => deleteContact(contact.id)}
-                      className="delete-button"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </>
-              )}
+              <td>{contact.firstName}</td>
+              <td>{contact.lastName}</td>
+              <td>{contact.email}</td>
+              <td>{contact.phone}</td>
+              <td>{contact.message}</td>
+              <td>
+                <button
+                  onClick={() => deleteContact(contact.id)}
+                  className="delete-button"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
