4198e7a60610c57eec2967d6e47bec244d7ca830
Dhritishah03 <1032211764@mitwpu.in>
2023-07-14 17:54:16 +0530

    Dhriti

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -4,13 +4,13 @@ import 'firebase/compat/storage';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSuvPaa2E0BHD2JNr6MkRNG0edql55Y6s",
-  authDomain: "aadyaventure.firebaseapp.com",
-  projectId: "aadyaventure",
-  storageBucket: "aadyaventure.appspot.com",
-  messagingSenderId: "483864047538",
-  appId: "1:483864047538:web:5fa9c152776a3e35ad3eab",
-  measurementId: "G-71Q4K6JKZ6"
+  apiKey: "AIzaSyBysTUbGw8fEDVSqK1cvNWZMwo2zkSUpRg",
+  authDomain: "aadyaventures-15e61.firebaseapp.com",
+  projectId: "aadyaventures-15e61",
+  storageBucket: "aadyaventures-15e61.appspot.com",
+  messagingSenderId: "179309954582",
+  appId: "1:179309954582:web:93566c250a5d1043549737",
+  measurementId: "G-8FPWTS551Y"
 };
 
 firebase.initializeApp(firebaseConfig);
