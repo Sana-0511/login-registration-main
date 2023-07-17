85d86be1dcf2b3a208f876855ba8f192c8fe50ad
Dhritishah03 <1032211764@mitwpu.in>
2023-07-17 12:10:44 +0530

    Dhriti

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -4,13 +4,13 @@ import 'firebase/compat/storage';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBysTUbGw8fEDVSqK1cvNWZMwo2zkSUpRg",
-  authDomain: "aadyaventures-15e61.firebaseapp.com",
-  projectId: "aadyaventures-15e61",
-  storageBucket: "aadyaventures-15e61.appspot.com",
-  messagingSenderId: "179309954582",
-  appId: "1:179309954582:web:93566c250a5d1043549737",
-  measurementId: "G-8FPWTS551Y"
+  apiKey: "AIzaSyClBHlCXTlSzJZbd2VBSns2F_N4yMzc37g",
+  authDomain: "aadya-2b01d.firebaseapp.com",
+  projectId: "aadya-2b01d",
+  storageBucket: "aadya-2b01d.appspot.com",
+  messagingSenderId: "49500958596",
+  appId: "1:49500958596:web:7634b1c652c90ab8b8f7d1",
+  measurementId: "G-VJTCTS45LD"
 };
 
 firebase.initializeApp(firebaseConfig);
